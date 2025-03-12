095fc8253953cf7b29d3e3753b3c68ce4598192a
Lau Heng Yi <77540391+lauhengyi@users.noreply.github.com>
2025-03-12 23:49:02 +0800

    Added job and status to UI

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -39,6 +39,10 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private Label email;
     @FXML
+    private Label jobPosition;
+    @FXML
+    private Label status;
+    @FXML
     private FlowPane tags;
 
     /**
@@ -52,6 +56,8 @@ public class PersonCard extends UiPart<Region> {
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
+        jobPosition.setText(person.getJobPosition().jobPosition);
+        status.setText(person.getStatus().status);
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
