ea4a6bb9f2cde3030c5913156d27d9a36ee2b1ed
Gabriella <gabriella@u.nus.edu>
2025-03-25 21:52:16 +0800

    Add parser for ExportCommand

---
+++ src/main/java/seedu/address/logic/parser/ExportCommandParser.java
@@ -0,0 +1,22 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.logic.commands.ExportCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new ExportCommand object.
+ */
+public class ExportCommandParser implements Parser<ExportCommand> {
+
+    @Override
+    public ExportCommand parse(String args) throws ParseException {
+        String trimmed = args.trim();
+        if (trimmed.isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ExportCommand.MESSAGE_USAGE));
+        }
+
+        return new ExportCommand(trimmed);
+    }
+}
