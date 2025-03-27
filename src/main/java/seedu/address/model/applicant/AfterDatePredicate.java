e0a74cc52bcad177f3e310e2b799ba96631318ba
yuqiannemo <e1300534@u.nus.edu>
2025-03-27 10:13:33 +0800

    Add predicates for dates

--- src/main/java/seedu/address/model/applicant/AfterDatePredicate.java
+++ src/main/java/seedu/address/model/applicant/AfterDatePredicate.java
@@ -1,4 +1,26 @@
 package seedu.address.model.applicant;
 
-public class AfterDatePredicate {
+import java.time.LocalDateTime;
+
+/**
+ * Represents a predicate that tests whether an {@code Applicant}'s application date
+ * is after the specified date.
+ */
+public class AfterDatePredicate extends IdentifierPredicate {
+    private final String afterDate;
+
+    public AfterDatePredicate(String afterDate) {
+        super(afterDate);
+        this.afterDate = afterDate;
+    }
+
+    @Override
+    public boolean test(Applicant applicant) {
+        return applicant.getAddedTime().isAfter(LocalDateTime.parse(afterDate));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other instanceof AfterDatePredicate && afterDate.equals(((AfterDatePredicate) other).afterDate);
+    }
 }
--- src/main/java/seedu/address/model/applicant/BeforeDatePredicate.java
+++ src/main/java/seedu/address/model/applicant/BeforeDatePredicate.java
@@ -1,4 +1,27 @@
 package seedu.address.model.applicant;
 
-public class BeforeDatePredicate {
+import java.time.LocalDateTime;
+
+/**
+ * Represents a predicate that tests whether an {@code Applicant}'s application date
+ * is before the specified date.
+ */
+public class BeforeDatePredicate extends IdentifierPredicate {
+    private final String beforeDate;
+
+    // todo: this code seems problematic
+    public BeforeDatePredicate(String beforeDate) {
+        super(beforeDate);
+        this.beforeDate = beforeDate;
+    }
+
+    @Override
+    public boolean test(Applicant applicant) {
+        return applicant.getAddedTime().isBefore(LocalDateTime.parse(beforeDate));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other instanceof BeforeDatePredicate && beforeDate.equals(((BeforeDatePredicate) other).beforeDate);
+    }
 }
