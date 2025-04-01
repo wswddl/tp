fcf27be62ae42edc1e110abe2a7039a8b76364be
Gabriella <gabriella@u.nus.edu>
2025-04-01 15:27:53 +0800

    Add addedtime and rating field to export csv

--- src/main/java/seedu/address/logic/commands/ExportCommand.java
+++ src/main/java/seedu/address/logic/commands/ExportCommand.java
@@ -1,10 +1,13 @@
 package seedu.address.logic.commands;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.applicant.Applicant;
@@ -21,10 +24,10 @@ public class ExportCommand extends Command {
             + "Example: " + COMMAND_WORD + " applicants_data.csv";
 
     public static final String MESSAGE_SUCCESS = "Exported applicant list to: %s";
+    public static final String MESSAGE_CANCELLED = "Export cancelled.";
     public static final String MESSAGE_FAILURE = "Failed to export applicant list.";
 
     private final String fileName;
-
     /**
      * Constructs an {@code ExportCommand} with the given file name.
      *
@@ -42,7 +45,7 @@ public class ExportCommand extends Command {
     public String getFileName() {
         return fileName;
     }
-    
+
     /**
      * Executes the export command by writing the currently filtered applicant list to a CSV file.
      *
@@ -52,28 +55,46 @@ public class ExportCommand extends Command {
      */
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        List<Applicant> applicants = model.getFilteredPersonList();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save Exported CSV");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSV Files", "*.csv"));
+        fileChooser.setInitialFileName(fileName);
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
 
-        try (FileWriter writer = new FileWriter(fileName)) {
-            writer.write("Name,Email,Phone,Job Position,Status,Tags\n");
+        File file = fileChooser.showSaveDialog(new Stage()); // open new dialog stage
 
+        if (file == null) {
+            return new CommandResult(MESSAGE_CANCELLED);
+        }
+
+        if (!file.getName().endsWith(".csv")) {
+            file = new File(file.getAbsolutePath() + ".csv");
+        }
+
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.write("Name,Email,Phone,Address,Job Position,Status,Tags,Added Time,Rating\n");
+
+            List<Applicant> applicants = model.getFilteredPersonList();
             for (Applicant a : applicants) {
-                writer.write(String.format("\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"\n",
+                writer.write(String.format("\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"\n",
                         a.getName().fullName,
                         a.getEmail().value,
                         a.getPhone().value,
+                        a.getAddress().value,
                         a.getJobPosition().jobPosition,
                         a.getStatus().value,
-                        a.getTags().stream().map(tag -> tag.tagName).collect(Collectors.joining(";"))
+                        a.getTags().stream().map(tag -> tag.tagName).collect(Collectors.joining(";")),
+                        a.getAddedTime().toString(),
+                        a.getRating().toString()
                 ));
             }
 
-            return new CommandResult(String.format(MESSAGE_SUCCESS, fileName));
+            return new CommandResult(String.format(MESSAGE_SUCCESS, file.getName()));
         } catch (IOException e) {
-            throw new CommandException(MESSAGE_FAILURE);
+            throw new CommandException(MESSAGE_FAILURE, e);
         }
     }
-
+    
     /**
      * Checks whether this command is equal to another.
      *
